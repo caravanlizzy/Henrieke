8d496478105f0ec857e9a07b2b72fbf3c44890fd
caravanlizzy <marc.lambrecht@gmx.de>
2021-03-31 14:17:24 +0200

    split game class and main (to run game)

---
+++ python/main.py
@@ -0,0 +1,16 @@
+import game 
+
+print(game)
+game = Game()
+game.addplayer("Niklas")
+game.addplayer("Peter")
+game.addplayer("Franz")
+game.addplayer("Nata")
+game.addplayer("Henrieke")
+game.addplayer("Klaus")
+game.addplayer("Bea")
+game.addplayer("Hubertus")
+game.addplayer("YOU", "human")
+
+#%%
+game.startgame()
